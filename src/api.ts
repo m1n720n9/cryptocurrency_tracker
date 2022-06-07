7e2ac9a3efa5253f9a0d8418790e936bbc41bdaa
m1n720n9 <skmnjng@gmail.com>
2022-06-07 16:02:29 +0900

    Fix chart api

--- src/api.ts
+++ src/api.ts
@@ -15,6 +15,6 @@ export function fetchCoinTickers(coinId : string) {
 
 export function fetchCoinHistory(coinId:string){
   const endDate = Math.floor(Date.now()/1000);
-  const startDate = endDate - 60*60*24*7*2;
+  const startDate = endDate - 60*60*24*6;
   return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`).then(response => response.json())
 }
